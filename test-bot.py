da76f5da5a3003bd053249c7e6d9cde73678ee02
Piyush Agrawal <piyush200205@gmail.com>
2021-11-13 13:35:11 +0530

    final commit

--- test-bot.py
+++ test-bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import youtube_dl
 import os
 
-client = commands.Bot(command_prefix="?")
+client = commands.Bot(command_prefix="!")
 
 @client.command()
 async def play(ctx, url : str):
